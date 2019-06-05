707846cd4a79240bc5881228ed6d78830aaef161
Liam Sullivan <30022060@student.toiohomai.ac.nz>
2019-06-05 12:37:32 +1200

    yesn't

---
+++ Assignment/Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        //Liam Sullivan ID:30022060 COMP.5202 Assignment
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("****ROTORUA THERMAL HOLIDAY PARK****\n------------------------------------");
+            Console.Write("\nPlease enter your Username: ");
+            string user = Console.ReadLine();
+            Console.Write("Please enter a password: ");
+            string pass = Console.ReadLine();
+
+            bool loop = true;
+            {
+                Console.Write($"Welcome {user}, please enter your password again: ");
+                string check = Console.ReadLine();
+
+                             
+            }   while (loop);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("****APPROVED****");
+                Console.ReadLine();
+            
+            
+        } 
+         
+    }
+}
