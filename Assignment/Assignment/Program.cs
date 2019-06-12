ae93333fab4a63d94426711a41b9d640eefcea5f
Liam Sullivan <30022060@student.toiohomai.ac.nz>
2019-06-13 10:13:15 +1200

    Assignment

--- Assignment/Assignment/Program.cs
+++ Assignment/Assignment/Program.cs
@@ -17,16 +17,34 @@ namespace Assignment
             string user = Console.ReadLine();
             Console.Write("Please enter a Password: ");
             string pass = Console.ReadLine();
-            
-            do
+
+            Console.Write("Please type your username again: ");
+            string input = Console.ReadLine();
+            while(input != user)
             {
-              Console.WriteLine("Please type your username again");
-              string input = Console.ReadLine();
-              
-            } while (user != input);
-
-
-            Console.WriteLine("****APPROVED****");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("ERROR. USERNAME DOES NOT MATCH.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\nPlease type your username again: ");
+                input = Console.ReadLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("USERNAME SET!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Please type your password again: ");
+            string input2 = Console.ReadLine();
+            while (input2 != pass)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("ERROR. PASSWORD DOES NOT MATCH.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\nPlease type your password again: ");
+                input2 = Console.ReadLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("PASSWORD SET!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n****APPROVED USER****");
             Console.ReadLine();
             
             
